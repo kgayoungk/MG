42c75fe86cd4d508adab8b641b234da7c1f4a900
kgayoungk <kgayoungk@gmail.com>
2018-10-26 21:06:03 +0900

    modify main and intro activities

--- app/src/main/java/com/example/forand/myogyeong/Intro.java
+++ app/src/main/java/com/example/forand/myogyeong/Intro.java
@@ -1,13 +1,52 @@
 package com.example.forand.myogyeong;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.os.Handler;
 
 public class Intro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 화면 가로 고정
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // 상태바 제거
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_intro);
+
+        // 인트로 애니메이션
+        TextView intro_txt1 = (TextView) findViewById(R.id.intro_txt1);
+        Animation intro_anim1 = AnimationUtils.loadAnimation(this, R.anim.intro_anim);
+        intro_txt1.startAnimation(intro_anim1);
+
+        TextView intro_txt2 = (TextView) findViewById(R.id.intro_txt2);
+        Animation intro_anim2 = AnimationUtils.loadAnimation(this, R.anim.intro_anim2);
+        intro_txt2.startAnimation(intro_anim2);
+
+        // 화면 자동 전환
+        Handler myHandler = new Handler();
+        myHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i2 = new Intent(Intro.this, SajeongIn.class);
+                startActivity(i2);
+                finish();
+            }
+        }, 13500);
+
+        // 배경음악
     }
+
 }
--- app/src/main/java/com/example/forand/myogyeong/MainActivity.java
+++ app/src/main/java/com/example/forand/myogyeong/MainActivity.java
@@ -3,15 +3,22 @@ package com.example.forand.myogyeong;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Typeface;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton pat_cat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +33,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 고양이 쓰다듬기
-        ImageButton pat_cat = (ImageButton) findViewById(R.id.cat1_main);
+        pat_cat = (ImageButton) findViewById(R.id.cat1_main);
         pat_cat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), SajeongIn.class);
-                startActivity(intent);
+                Intent i1 = new Intent(getApplicationContext(), Intro.class);
+                startActivity(i1);
             }
         });
 
+        // 타이틀 애니메이션
+        ImageView title = (ImageView) findViewById(R.id.title);
+        Animation title_anim = AnimationUtils.loadAnimation(this, R.anim.main_title_anim);
+        title.startAnimation(title_anim);
+
+        // 쓰다듬어 주세요 애니메이션
+        TextView pat = (TextView) findViewById(R.id.patme);
+        Animation pat_anim = AnimationUtils.loadAnimation(this, R.anim.main_pat_anim);
+        pat.startAnimation(pat_anim);
+
+        // 고양이 애니메이션
+        Animation cat_anim = AnimationUtils.loadAnimation(this, R.anim.main_cat_anim);
+        cat_anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Animation cat_anim_move = AnimationUtils.loadAnimation(MainActivity.this, R.anim.main_cat_anim_move);
+                pat_cat.startAnimation(cat_anim_move);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        pat_cat.startAnimation(cat_anim);
+
         // 배경음악
+
     }
 
 }
